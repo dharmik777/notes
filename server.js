2db9d84d85bb8058b339182d1812e44343d82d84
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-28 22:31:37 -0500

    .

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
-// Dependencies
-const express = require("express");
-const path = require('path');
+const express = require ("express");
+const path = require ("path");
 const fs = require("fs");
 
 
+const PORT = 8080;
 
-//Start Express APP 
 const app = express();
-const PORT = process.env.PORT || 8080;
-
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
+require("./routes/apiroutes.js")(app);
+require("./routes/htmlroutes.js")(app);
 
-// Link to ROUTE Files
-require('./api.js')(app);
-
-
-// Start listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});  
+app.listen(PORT, function (){
+    console.log("its working!")
+});
